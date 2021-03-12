af19840adf2c04ce16be06d2f044fc3f81ecbaac
Raider992 <19stargazer92@gmail.com>
2021-03-12 19:43:30 +0300

    Дз-3 нулевой коммит

--- backend/todo_service/library/serializers.py
+++ backend/todo_service/library/serializers.py
@@ -1,8 +1,12 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework import serializers
+
 from library.models import Author
 
 
-class AuthorModelSerializer(HyperlinkedModelSerializer):
+class AuthorModelSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Author
         fields = '__all__'
+
+class ArticleSerializer(serializers.ModelSerializer):
+    author = AuthorModelSerializer()
